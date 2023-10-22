ee435b0d0867d967953e0562bddbf723e02ef1e6
tchan24 <tchan0524@gmail.com>
2023-10-22 18:54:54 -0500

    modified:   HotelSimulation.java

--- HotelSimulation.java
+++ HotelSimulation.java
@@ -5,24 +5,25 @@ public class HotelSimulation {
 
     public static class Bellhop implements Runnable {
         private int id;
-        private Semaphore bellhopSemaphore;
+        private static Semaphore bellhopSemaphore = new Semaphore(0);  // Initialize to 0 so bellhops wait
         private static ArrayList<Guest> guestsNeedingBellhop = new ArrayList<>();
 
         public Bellhop(int id) {
             this.id = id;
-            this.bellhopSemaphore = new Semaphore(1);
         }
 
         @Override
         public void run() {
             try {
                 while (true) {
-                    bellhopSemaphore.acquire();
+                    bellhopSemaphore.acquire();  // Wait for a guest
                     Guest guest = getGuestRequiringBellhop();
                     if (guest == null) continue;  // No guest found, continue to next iteration
 
                     System.out.println("Bellhop " + id + " receives bags from guest " + guest.getId());
-                    guest.getRoomSemaphore().acquire();
+            // Simulate the bellhop taking some time to deliver the bags
+                    Thread.sleep(1000);  // 1 second delay for example
+                    guest.getRoomSemaphore().release();
                     System.out.println("Bellhop " + id + " delivers bags to guest " + guest.getId());
                     System.out.println("Guest " + guest.getId() + " receives bags from bellhop " + id + " and gives tip");
                     guest.getBagSemaphore().release();
@@ -31,6 +32,7 @@ public class HotelSimulation {
                 Thread.currentThread().interrupt();
             }
         }
+
 
         public static synchronized Guest getGuestRequiringBellhop() {
             if (!guestsNeedingBellhop.isEmpty()) {
@@ -41,6 +43,7 @@ public class HotelSimulation {
 
         public static synchronized void addGuestRequiringBellhop(Guest guest) {
             guestsNeedingBellhop.add(guest);
+            bellhopSemaphore.release();  // Notify a bellhop that a guest is waiting
         }
     }
 
@@ -80,9 +83,8 @@ public class HotelSimulation {
         for (int i = 0; i < 25; i++) {
             Guest guest = new Guest(i);
             guests.add(guest);
-            
+
             System.out.println("Guest " + guest.getId() + " created");
-            // Some simulated operations here...
             Bellhop.addGuestRequiringBellhop(guest);  // Add guest for bellhop assistance
         }
 
